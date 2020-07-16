b901ee94a42731d07f1409b1fe3464a7d2c6e9b1
marat <marat@brainway.co.il>
2020-07-16 09:56:31 +0300

    a lot of changes

--- src/input-moment.js
+++ src/input-moment.js
@@ -3,17 +3,23 @@ import moment from 'moment';
 import React, { Component } from 'react';
 import Calendar from './calendar';
 import Time from './time';
+import time from './time';
 
 export default class InputMoment extends Component {
   static defaultProps = {
     prevMonthIcon: 'ion-ios-arrow-left',
     nextMonthIcon: 'ion-ios-arrow-right',
+    dateIcon: 'ion-calendar im-btn',
+    timeIcon: 'ion-clock im-btn',
+    onValidate: m => true,
+    customText: {},
     minStep: 1,
-    hourStep: 1
+    hourStep: 1,
   };
 
   state = {
-    tab: 0
+    tab: 0,
+    _moment: this.props.moment.clone(),
   };
 
   handleClickTab = (e, tab) => {
@@ -23,7 +29,16 @@ export default class InputMoment extends Component {
 
   handleSave = e => {
     e.preventDefault();
+    if (this.state.tab === 0) {
+      this.setState({ tab: 1 });
+      return;
+    }
+    this.props.onChange(this.props.moment.clone())
     if (this.props.onSave) this.props.onSave();
+  };
+
+  handleChange = _moment => {
+    this.setState({ _moment: _moment.clone() });
   };
 
   render() {
@@ -33,46 +48,68 @@ export default class InputMoment extends Component {
       className,
       prevMonthIcon,
       nextMonthIcon,
+      dateIcon,
+      timeIcon,
       minStep,
       hourStep,
       onSave,
+      customText,
+      onValidate,
       ...props
     } = this.props;
     const cls = cx('m-input-moment', className);
+    const today = moment().date();
+
+    const text = {
+      date: 'Date',
+      time: 'Time',
+      next: 'Next',
+      save: 'Save',
+      hours: 'Hours:',
+      minutes: 'Minutes:',
+      ...customText
+    }
 
     return (
       <div className={cls} {...props}>
         <div className="options">
           <button
             type="button"
-            className={cx('ion-calendar im-btn', { 'is-active': tab === 0 })}
+            className={cx(dateIcon, { 'is-active': tab === 0 })}
             onClick={e => this.handleClickTab(e, 0)}
           >
-            Date
+            {text.date}
           </button>
           <button
             type="button"
-            className={cx('ion-clock im-btn', { 'is-active': tab === 1 })}
+            className={cx(timeIcon, { 'is-active': tab === 1 })}
             onClick={e => this.handleClickTab(e, 1)}
           >
-            Time
+            {text.time}
           </button>
         </div>
 
         <div className="tabs">
           <Calendar
             className={cx('tab', { 'is-active': tab === 0 })}
-            moment={m}
-            onChange={this.props.onChange}
+            _moment={this.state._moment}
+            moment={this.props.moment}
+            onChange={this.handleChange}
             prevMonthIcon={this.props.prevMonthIcon}
             nextMonthIcon={this.props.nextMonthIcon}
+            today={today}
+            onSetDate={this.props.onChange}
+            onValidate={onValidate}
           />
           <Time
             className={cx('tab', { 'is-active': tab === 1 })}
-            moment={m}
+            _moment={this.state._moment}
+            moment={this.props.moment}
             minStep={this.props.minStep}
             hourStep={this.props.hourStep}
             onChange={this.props.onChange}
+            text={text}
+            onValidate={onValidate}
           />
         </div>
 
@@ -82,7 +119,7 @@ export default class InputMoment extends Component {
             className="im-btn btn-save ion-checkmark"
             onClick={this.handleSave}
           >
-            Save
+            {!this.state.tab && text.next || text.save}
           </button>
         ) : null}
       </div>
